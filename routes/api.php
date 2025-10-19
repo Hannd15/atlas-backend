3ea63bb30ee29b870c71c03d48243c121f17e6ea
Hannd15 <lejhernandez08@gmail.com>
2025-10-19 17:47:32 -0500

    Arregla manejo erróneo de las rutas Dropdown

--- routes/api.php
+++ routes/api.php
@@ -5,31 +5,31 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-//Route::middleware('auth:sanctum')->group(function () {
+// Route::middleware('auth:sanctum')->group(function () {
 
-    Route::prefix('/auth')->group(function () {
-        // User routes
-        Route::get('/users', [UserController::class, 'index']);
-        Route::post('/users', [UserController::class, 'store']);
-        Route::get('/users/{id}', [UserController::class, 'show']);
-        Route::put('/users/{id}', [UserController::class, 'update']);
-        Route::delete('/users/{id}', [UserController::class, 'destroy']);
-        Route::get('/users/dropdown', [UserController::class, 'dropdown']);
-    
-        // Role routes
-        Route::get('/roles', [RoleController::class, 'index']);
-        Route::post('/roles', [RoleController::class, 'store']);
-        Route::get('/roles/{id}', [RoleController::class, 'show']);
-        Route::put('/roles/{id}', [RoleController::class, 'update']);
-        Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
-        Route::get('/roles/dropdown', [RoleController::class, 'dropdown']);
-    
-        // Permission routes
-        Route::get('/permissions', [PermissionController::class, 'index']);
+Route::prefix('/auth')->group(function () {
+    // User routes
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/dropdown', [UserController::class, 'dropdown']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+    // Role routes
+    Route::get('/roles', [RoleController::class, 'index']);
+    Route::post('/roles', [RoleController::class, 'store']);
+    Route::get('/roles/dropdown', [RoleController::class, 'dropdown']);
+    Route::get('/roles/{id}', [RoleController::class, 'show']);
+    Route::put('/roles/{id}', [RoleController::class, 'update']);
+    Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
+
+    // Permission routes
+    Route::get('/permissions', [PermissionController::class, 'index']);
     Route::post('/permissions', [PermissionController::class, 'store']);
-        Route::get('/permissions/{id}', [PermissionController::class, 'show']);
-        Route::put('/permissions/{id}', [PermissionController::class, 'update']);
-        Route::delete('/permissions/{id}', [PermissionController::class, 'destroy']);
-        Route::get('/permissions/dropdown', [PermissionController::class, 'dropdown']);
-    });
-//});
+    Route::get('/permissions/dropdown', [PermissionController::class, 'dropdown']);
+    Route::get('/permissions/{id}', [PermissionController::class, 'show']);
+    Route::put('/permissions/{id}', [PermissionController::class, 'update']);
+    Route::delete('/permissions/{id}', [PermissionController::class, 'destroy']);
+});
+// });
